0abfefd407a7d39ee2f2200b081c56fc9fbd5f1b
Andy Zhang <signupforandy@gmail.com>
2016-08-05 17:45:46 -0700

    Run 2to3 on q2_sigmoid

--- assignment1/q2_sigmoid.py
+++ assignment1/q2_sigmoid.py
@@ -32,17 +32,17 @@ def test_sigmoid_basic():
     Some simple tests to get you started. 
     Warning: these are not exhaustive.
     """
-    print "Running basic tests..."
+    print("Running basic tests...")
     x = np.array([[1, 2], [-1, -2]])
     f = sigmoid(x)
     g = sigmoid_grad(f)
-    print f
+    print(f)
     assert np.amax(f - np.array([[0.73105858, 0.88079708],
                                  [0.26894142, 0.11920292]])) <= 1e-6
-    print g
+    print(g)
     assert np.amax(g - np.array([[0.19661193, 0.10499359],
                                  [0.19661193, 0.10499359]])) <= 1e-6
-    print "You should verify these results!\n"
+    print("You should verify these results!\n")
 
 
 def test_sigmoid():
@@ -52,18 +52,18 @@ def test_sigmoid():
     This function will not be called by the autograder, nor will
     your tests be graded.
     """
-    print "Running your tests..."
+    print("Running your tests...")
     ### YOUR CODE HERE
     x = np.array([[0, 0, 0]])
     f = sigmoid(x)
     g = sigmoid_grad(f)
 
-    print f
+    print(f)
     assert np.all(np.equal(f, np.array([
         [.5, .5, .5],
     ])))
-    print "gradient"
-    print g
+    print("gradient")
+    print(g)
     assert np.all(np.equal(g, np.array([
         [.25, .25, .25]
     ])))
