a84e806e161fc9234b9e83bb87ffc8d211ff93e6
Andy Zhang <signupforandy@gmail.com>
2016-08-11 18:43:25 -0700

    Run 2to3 on q3_run

--- assignment1/q3_run.py
+++ assignment1/q3_run.py
@@ -21,13 +21,13 @@ C = 5
 # Reset the random seed to make sure that everyone gets the same results
 random.seed(31415)
 np.random.seed(9265)
-wordVectors = np.concatenate(((np.random.rand(nWords, dimVectors) - .5) / \
+wordVectors = np.concatenate(((np.random.rand(nWords, dimVectors) - .5) /
 	dimVectors, np.zeros((nWords, dimVectors))), axis=0)
 wordVectors0 = sgd(
     lambda vec: word2vec_sgd_wrapper(skipgram, tokens, vec, dataset, C, 
     	negSamplingCostAndGradient), 
     wordVectors, 0.3, 40000, None, True, PRINT_EVERY=10)
-print "sanity check: cost at convergence should be around or below 10"
+print("sanity check: cost at convergence should be around or below 10")
 
 # sum the input and output word vectors
 wordVectors = (wordVectors0[:nWords,:] + wordVectors0[nWords:,:])
@@ -46,7 +46,7 @@ covariance = 1.0 / len(visualizeIdx) * temp.T.dot(temp)
 U,S,V = np.linalg.svd(covariance)
 coord = temp.dot(U[:,0:2]) 
 
-for i in xrange(len(visualizeWords)):
+for i in range(len(visualizeWords)):
     plt.text(coord[i,0], coord[i,1], visualizeWords[i], 
     	bbox=dict(facecolor='green', alpha=0.1))
     
